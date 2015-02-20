d9c133ea80d2916ba12001de3a0befb92eefc226
Sebastian Bromberg <seba@wagaduu.com>
2015-02-20 10:43:17 -0300

    Encode Working. XML BAD error

--- builders/cmsBuilder.js
+++ builders/cmsBuilder.js
@@ -4,7 +4,6 @@
 var ticketFactory = require("../factories/ticketFactory");
 var logger = require("winston");
 var path = require('path');
-var fs = require('fs');
 
 exports.CmsBuilder = function (companyId, callback) {
     var self = this;
@@ -19,19 +18,25 @@ exports.CmsBuilder = function (companyId, callback) {
 
     function createTicket(callback) {
         ticket = ticketFactory.createTicket("30700627825", "30700627825", "wsfe", 3600);
-        var sha1 = require('sha1');
-        fs.writeFile(path.join(__dirname, "TRA.xml"), sha1(ticket), function (err) {
-            callback(err);
-        });
+        callback();
+        //var sha1 = require('sha1');
+        //fs.writeFile(path.join(__dirname, "TRA.xml"), ticket, function (err) {
+        //    callback(err);
+        //});
     }
 
     function signTicket(callback) {
         logger.info("signTicket --------------- ");
 
-        var smime = require('smime');
+        var signHelper = require('../helpers/signHelper');
+        var Readable = require('stream').Readable;
 
-        smime.sign({
-            content: fs.createReadStream(path.join(__dirname, "TRA.xml")),
+        var s = new Readable();
+        s.push(ticket);    // the string you want
+        s.push(null);      // indicates end-of-file basically - the end of the stream
+
+        signHelper.sign({
+            content: s, //fs.createReadStream(path.join(__dirname, "TRA.xml")),
             key: path.join(__dirname, '../certs/test/' + "30700627825" + ".privada"),
             cert: path.join(__dirname, '../certs/test/' + "30700627825" + ".crt"),
             password: '30700627825'
@@ -42,11 +47,16 @@ exports.CmsBuilder = function (companyId, callback) {
         }).then(function (result) {
             //logger.info("Signed Response: ");
             //console.dir(result); // {der, child}
+
+            //var cmsFile = fs.readFileSync(path.join(__dirname, "30700627825.der4"));
+            //cmsMessage = cmsFile;
             cmsMessage = result.der;
+            //fs.writeFileSync( path.join(__dirname, "30700627825.der"), result.der, "binary");
             //logger.info("CMS Obtenido: " + res.der);
             logger.info("signTicket ++++++++++++");
-            callback(null, result);
+            callback(null);
         });
+
 
         //cmsMessage = "-----BEGIN CMS-----        MIIGsgYJKoZIhvcNAQcCoIIGozCCBp8CAQExCzAJBgUrDgMCGgUAMIIBeQYJKoZI        hvcNAQcBoIIBagSCAWY8P3htbCB2ZXJzaW9uPSIxLjAiIGVuY29kaW5nPSJVVEYt        OCIgc3RhbmRhbG9uZT0ieWVzIj8+PGxvZ2luVGlja2V0UmVxdWVzdCB2ZXJzaW9u        PSIxLjAiPjxoZWFkZXI+PHNvdXJjZT51bmRlZmluZWQ8L3NvdXJjZT48ZGVzdGlu        YXRpb24+bmkgaWRlYTwvZGVzdGluYXRpb24+PHVuaXF1ZUlkPjE0MjM2ODkxNDEy        MTE8L3VuaXF1ZUlkPjxnZW5lcmF0aW9uVGltZT4yMDE1LTAyLTExVDIxOjEyOjIx        LjIxMVo8L2dlbmVyYXRpb25UaW1lPjxleHBpcmF0aW9uVGltZT4yMDE1LTAyLTEx        VDIxOjEyOjIxLjIxMVo8L2V4cGlyYXRpb25UaW1lPjwvaGVhZGVyPjxzZXJ2aWNl        PndzZmU8L3NlcnZpY2U+PC9sb2dpblRpY2tldFJlcXVlc3Q+oIIDtjCCA7IwggKa        oAMCAQICCAvBvKCQBKw9MA0GCSqGSIb3DQEBBQUAMEMxJTAjBgNVBAMMHEFGSVAg        VGVzdGluZyBDb21wdXRhZG9yZXMgQ0ExDTALBgNVBAoMBEFGSVAxCzAJBgNVBAYT        AkFSMB4XDTE0MTEwNTEzNDIxM1oXDTE3MDgwMTEzNDIxM1owUDEQMA4GA1UEAwwH        ZmludGVzdDEZMBcGA1UEBRMQQ1VJVCAzMDcwMDYyNzgyNTEUMBIGA1UECgwLUml0        ZW5lcmUgU0ExCzAJBgNVBAYTAkFSMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB        gQCnRguhpnplyDk+r+VnZk0ND+oBMHp3x5z+Ut9TlEq9uWb7fekNAX/Gt2xW3E0h        yqFhyP7LQs473dqgExmliMTncxWH/b7jehYr1hZQlYe65/QqmbnziuARf7x6vTfn        63qii6/OtxDXjpFKKYWBwxhavKeF3BizydihvfbZuhS8wwIDAQABo4IBHzCCARsw        DAYDVR0TAQH/BAIwADAOBgNVHQ8BAf8EBAMCBeAwHQYDVR0OBBYEFMcyDkOWSPuF        nApWXrumtTwRTN3uMB8GA1UdIwQYMBaAFER07rScJt9W4cEN61cLkBk2PZYBMIG6        BgNVHSAEgbIwga8wgawGDisGAQQBgbtjAQIBAgEBMIGZMIGWBggrBgEFBQcCAjCB        iR6BhgBDAGUAcgB0AGkAZgBpAGMAYQBkAG8AIABwAGEAcgBhACAAYwBvAG0AcAB1        AHQAYQBkAG8AcgBlAHMAIABzAG8AbABvACAAdgBhAGwAaQBkAG8AIABlAG4AIABl        AG4AdABvAHIAbgBvAHMAIABkAGUAIABkAGUAcwBhAHIAcgBvAGwAbABvMA0GCSqG        SIb3DQEBBQUAA4IBAQB3L/krNCTDOOMCYR1STCd578RasGgtTJEs7qZBC+C4PNC0        ivjBxZQuye6gXc+g32FdyAppbXoSzldfk8/NKP2qrl5eMSJbOvU2/xmjyMkpm87A        sjQl88jsu+NNegDEh2tGZFguU8ZL/tl4MLw4VEJOqE7l6Vap348cz0zwYMoUaBsh       taltGIiwcqD5qJ4FU5Dc6S+j+gb7gw0j6rLII51w9lfYV0AiBAGTF0v5FAujmkEv        uMZgO1JBrANjgNFStpxgm+RpoGk9lV7671GqKgIVMWT16UlwCOzny24bKcOGJ37i        EXewoCfdLzD3PywVoH0OzgK/V/+n7rgKlzwKqSsGMYIBVDCCAVACAQEwTzBDMSUw        IwYDVQQDDBxBRklQIFRlc3RpbmcgQ29tcHV0YWRvcmVzIENBMQ0wCwYDVQQKDARB        RklQMQswCQYDVQQGEwJBUgIIC8G8oJAErD0wCQYFKw4DAhoFAKBdMBgGCSqGSIb3        DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTE1MDIxMTIzMTc0Mlow        IwYJKoZIhvcNAQkEMRYEFF/04Wfh/0sVd/aDAQLADvuPLL9EMA0GCSqGSIb3DQEB        BQUABIGAAi1D4PhKInqTpGQi2hai6eju5Db5S+pRVLX5N4coDY1s6+tHyun7tm87        iajydn7t3hfPrV7eAhnZxuYY9BmjQ4EYLzxijFdKaZzcIbdq/90erUpFoA6Atb3h        mb/H0NnCM/Nio12HsH1Q/AiOW7MYQkUnvir7lFZ74WN798+k8eA=            -----END CMS-----       ";
 
@@ -114,14 +124,13 @@ exports.CmsBuilder = function (companyId, callback) {
             if (err) {
                 return (err);
             }
-            signTicket(function (err, result) {
+            signTicket(function (err) {
                 if (err) {
                     return (err);
                 }
-                cmsMessage = result.der;
                 encodeCms();
-                invokeAfipWSAA(function (err, ticket) {
-                    callback(err, ticket);
+                invokeAfipWSAA(function (err, ticketResponse) {
+                    callback(err, ticketResponse);
                 });
 
             });
--- factories/ticketFactory.js
+++ factories/ticketFactory.js
@@ -43,29 +43,31 @@ exports.createTicket = function createTicket(signerDN, dstDN, service, TicketTim
     //XMLGregorianCalendarImpl XMLExpTime = new XMLGregorianCalendarImpl(exptime);
     logger.info("Fecha: " + dateNow.getTime());
 
-    var loginTicketRequestXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
-        + "<loginTicketRequest version=\"1.0\">"
-        + "<header>"
-        + "<source>"
-        + "C=AR, O=Ritenere SA, SERIALNUMBER=CUIT 30700627825, CN=fintest"//signerDN
-        + "</source>"
-        + "<destination>"
-        + "CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239"//dstDN
-        + "</destination>"
-        + "<uniqueId>"
-        + uniqueId
-        + "</uniqueId>"
-        + "<generationTime>"
-        + gentime
-        + "</generationTime>"
-        + "<expirationTime>"
-        + exptime
-        + "</expirationTime>"
-        + "</header>"
-        + "<service>"
-        + service
-        + "</service>"
-        + "</loginTicketRequest>";
+    var loginTicketRequestXml = ' <?xml version="1.0" encoding="UTF-8"?> '
+            + '<loginTicketRequest> '
+            + '   <header>                                                                                    '
+            + '   <uniqueId>1423847</uniqueId>                                      '
+            + '    <generationTime>2015-02-13T14:13:40.332-03:00</generationTime>     '
+            + '<expirationTime>2015-02-13T15:13:40.332-03:00</expirationTime> '
+            + ' </header>               '
+            + ' <service>wsfe</service>'
+            + '</loginTicketRequest>'
+
+    /*         + ' <loginTicketRequest version="1.0"> '
+     + " <header> "
+     + " <uniqueId> " + uniqueId
+     + " </uniqueId> "
+     + " <generationTime> "
+     + gentime
+     + " </generationTime> "
+     + " <expirationTime> "
+     + exptime
+     + " </expirationTime> "
+     + " </header> "
+     + " <service> "
+     + service
+     + " </service> "
+     + " </loginTicketRequest> "*/;
 
     logger.info("Ticket: " + loginTicketRequestXml);
     logger.info("createTicket +++++");
